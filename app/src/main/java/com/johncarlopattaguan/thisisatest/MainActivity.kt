61177a519edbc21dc8ba3024e11677098dc7f856
akoosiicarlo123 <johncarlopattaguan@gmail.com>
2019-05-04 22:36:52 +0800

    new2

--- app/src/main/java/com/johncarlopattaguan/thisisatest/MainActivity.kt
+++ app/src/main/java/com/johncarlopattaguan/thisisatest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.johncarlopattaguan.thisisatest
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -20,6 +21,9 @@ class MainActivity : AppCompatActivity() {
 
             val message = myedittxt.text.toString()
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
 
 
         }
